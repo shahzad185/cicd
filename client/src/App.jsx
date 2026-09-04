c049c6a76a4a45e0632796813845faa152e52c34
Muhammed Shahzad <muhammed.shahzad@vodafone.com>
2026-09-04 11:09:41 +0100

    test: break the test now 123

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
 
   return (
     <main style={{ fontFamily: "system-ui", padding: "2rem" }}>
-      <h1>Compleltey different tile to break test</h1>
+      <h1>Compleltey different tile to break test 123</h1>
       <p>Clicked {count} times</p>
       <button onClick={() => setCount((c) => c + 1)}>Increment</button>
     </main>
